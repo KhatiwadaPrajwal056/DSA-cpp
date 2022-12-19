a77c201e5da4f3fd67e16d8c3cef18a7fe8b2e85
KhatiwadaPrajwal056 <khatiwadaprajwal22@icloud.com>
2022-12-19 18:21:12 +0545

    df

--- practice.cpp
+++ practice.cpp
@@ -3,7 +3,6 @@ You are given a task to develop a system to read at least 100 integers and conti
 user enters No. Your system must have the capacity to calculate the sum and average of those 
 numbers which are exactly divisible by 9 but not 6 and lies between 1 to 100 and display a
 suitable message if no such number is read. Write a code to develop such system. */
-
 #include<iostream>
 using namespace std;
 int main()
