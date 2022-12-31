c8175d22f4d07b5ef02cabbbdcf6e8db10ab873d
KhatiwadaPrajwal056 <khatiwadaprajwal22@icloud.com>
2022-12-31 14:15:26 +0545

    Evaluating postfix using stack

--- postfix.cpp
+++ postfix.cpp
@@ -1,68 +1,73 @@
-#include<iostream>
+//Evaluating postfix exression using stack 
+#include <iostream>
 #define size 10
 using namespace std;
-class postfix{
-    public:
-        float stack[size];
-        char postf[size];
-        int top;
-        postfix(){
-            top=-1;
-        }
-        void push(float);
-        float pop();
+class postfix
+{
+public:
+    float stack[size];
+    char postf[size];
+    int top;
+    postfix()
+    {
+        top = -1;
+    }
+    void push(float a)
+    {
+        stack[++top] = a;
+    }
+    float pop()
+    {
+        float a;
+        a = stack[top];
+        top--;
+        return a;
+    }
 };
-void postfix::push(float a){
-    stack[++top]=a;
-}
-float postfix::pop(){
-    float a;
-    a=stack[top];
-    top--;
-    return a;
-}
+
 int main()
-{   
-    float a,b,c;
+{
+    float a, b, c;
     postfix p;
-    int i=0;
-    cout<<"ENTER THE postfix exp: ";
-    cin>>p.postf;
-    while(p.postf[i]!='\0') {
-        switch (p.postf[i]){
-            case '+':
-                a=p.pop();
-                b=p.pop();
-                c=a+b;
-                p.push(c);
-                break;
-            case '-':
-                a=p.pop();
-                b=p.pop();
-                c=b-a;
-                p.push(c);
-                break;
+    int i = 0;
+    cout << "ENTER THE postfix exp: ";
+    cin >> p.postf;
+    while (p.postf[i] != '\0')
+    {
+        switch (p.postf[i])
+        {
+        case '+':
+            a = p.pop();
+            b = p.pop();
+            c = a + b;
+            p.push(c);
+            break;
+        case '-':
+            a = p.pop();
+            b = p.pop();
+            c = b - a;
+            p.push(c);
+            break;
 
-            case '*':
-                a=p.pop();
-                b=p.pop();
-                c=b*a;
-                p.push(c);
-                break;
+        case '*':
+            a = p.pop();
+            b = p.pop();
+            c = b * a;
+            p.push(c);
+            break;
 
-            case '/':
-                a=p.pop();
-                b=p.pop();
-                c=b/a;
-                p.push(c);
-                break;
-            default:
-                p.push(p.postf[i]-48);
-                break;
-            
+        case '/':
+            a = p.pop();
+            b = p.pop();
+            c = b / a;
+            p.push(c);
+            break;
+        default:
+            p.push(p.postf[i] - 48);
+            break;
         }
         i++;
     }
-    cout<<"Value in the stack is "<<p.stack[p.top]<<endl;
-    return 0 ;
+    cout << "Value in the stack is " << p.stack[p.top] << endl;
+    return 0;
 }
