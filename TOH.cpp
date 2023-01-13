2bbf7bd3e59c7de9028daddc8ae2adf500962295
KhatiwadaPrajwal056 <khatiwadaprajwal22@icloud.com>
2023-01-13 21:35:34 +0545

    tower of hanoi using recursion

--- TOH.cpp
+++ TOH.cpp
@@ -3,18 +3,18 @@
 using namespace std;
 void toh(int n,char src,char temp,char dest){
     if(n==1){
-        cout<<"Moved disk "<<n<<" from "<<src<<" to "<<dest<<endl;
+        cout<<"Move disk "<<n<<" from "<<src<<" to "<<dest<<endl;
     }
     else{
         toh(n-1,src,dest,temp);
-        cout<<"Moved disk "<<n<<" from "<<src<<" to "<<dest<<endl;
+        cout<<"Move disk "<<n<<" from "<<src<<" to "<<dest<<endl;
         toh(n-1,temp,src,dest);
     }
 }
 int main()
 {
     int n;
-    cout<<"Enter the number of disk:";
+    cout<<"Enter the number of disks:";
     cin>>n;
     toh(n,'a','b','c');
     return 0 ;
