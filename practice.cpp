2c894a07d618a034c54689d5ce5928d808a1cadc
KhatiwadaPrajwal056 <khatiwadaprajwal22@icloud.com>
2023-02-20 18:58:36 +0545

    practicing

--- practice.cpp
+++ practice.cpp
@@ -20,9 +20,101 @@ class node{
 
 };
 class tree{
-    
+    public:
+        node *newnode,*root;
+    tree(){
+        root=NULL;
+    }
+    void insertion(node *tree, int data){
+        newnode= new node;
+        newnode->info=data;
+        // newnode->left=NULL;
+        // newnode->right=NULL;
+
+        if(tree==NULL){
+            tree= newnode;
+            root=newnode;
+        
+        }
+        else if(data<tree->info){
+            if(tree->left==NULL){
+                tree->left=newnode;
+            }
+            else{
+                insertion(tree->left,data);
+            }
+        }
+        else if(data>tree->info){
+            if(tree->right==NULL){
+                tree->right=newnode;
+            }
+            else{
+                insertion(tree->right,data);
+            }
+        }
+        else
+            cout<<"double data"<<endl;
+    }
+    void preorder(node *tree){
+        if(tree!=NULL){
+            cout<<tree->info<<endl;
+            preorder(tree->left);
+            preorder(tree->right);
+        }
+    }
+    void postorder(node *tree){
+        if(tree!=NULL){
+            preorder(tree->left);
+            preorder(tree->right);
+            cout<<tree->info<<endl;
+        }
+    }
+    void inorder(node *tree){
+        if(tree!=NULL){
+            preorder(tree->left);
+            cout<<tree->info<<endl;
+            preorder(tree->right);
+        }
+    }
 };
 int main()
 {
+    tree p;
+    int x,toinsert;
+    while(1){
+        cout<<"1. Insertion"<<endl;
+        cout<<"2. preorder"<<endl;
+        cout<<"3. postorder"<<endl;
+        cout<<"4. inorder"<<endl;
+        cout<<"5. exit"<<endl;
+        cout<<"enter a choice: ";
+        cin>>x;
+        switch(x){
+            case 1:
+                cout<<"Enter the data to insert: ";
+                cin>>toinsert;
+                p.insertion(p.root,toinsert);
+                break;
+            case 2:
+                cout<<"Data in Preorder: "<<endl;
+                p.preorder(p.root);
+                break;                
+            case 3:
+                cout<<"Data in Postorder: "<<endl;
+                p.postorder(p.root);
+                break;
+            case 4:
+                cout<<"Data in Inorder: "<<endl;
+                p.inorder(p.root);
+                break;
+            case 5:
+                exit(0);
+                break;
+            default:
+                cout<<"Please enter the correct option: "<<endl;
+                break;
+
+        }
+    }
     return 0 ;
 }
